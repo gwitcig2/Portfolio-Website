35b8ac4f9a20c1de69a61d41f725144b8c54f93f
gwitcig2 <gwitcig2@huskers.unl.edu>
2025-09-03 00:07:24 -0500

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Portfolio Website
+
+Currently being built. Check back later to see some projects I've worked on, along with what I plan to build next!
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-[TASA Orbiter]">Under construction...</h1>
+  )
+}
+
+export default App
