49a357e9a21c3a30f6a1b92460e3bc310a216e33
gwitcig2 <gwitcig2@huskers.unl.edu>
2025-09-03 19:16:24 -0500

    Drafted the bio for the About Me page. Added social media links to the landing page

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,9 +1,34 @@
 import '../index.css'
 
+
+
 function AboutMe() {
+
+    const bday = new Date('June 10, 2003');
+    const now = new Date();
+
     return (
         <div>
             <h1>About Me</h1>
+            <p>
+                Hey there! The name's Gage. I'm currently {Math.abs(bday.getFullYear() - now.getFullYear())} years old, and I was born and raised in Lincoln, Nebraska.
+
+                I received my Bachelor of Science in Computer Science from the University of Nebraska-Lincoln in May of 2025. And since then, I've been on a mission to develop software that exemplifies passion, practicality, and creativity.
+
+                To me, video games are the best demonstrations of those three traits you can possibly ask for. They have been a big part of my life ever since my childhood, so
+                perhaps I'm a bit biased in saying that. But when story-rich, artistic masterpieces like OMORI and Red Dead Redemption 2 exist, it's hard not to be inspired by the impact
+                these video games had on me and millions of others out there. I hope to turn this inspiration of mine into a reality one day by founding my own indie game development studio. That way, I can fully commit
+                to creating an iconic, story-rich video game alongside my closest friends.
+
+                In the meantime, I've been building up my web development skills by creating an AI-powered Japanese Practice Website. I loved learning Japanese in college, and I wanted to continue learning about it outside of the classroom.
+                But after trying Duolingo and not being very satisfied with it, I figured now was a great time to leverage my coding skills, along with the teaching skills I earned as a Teaching Assistant, to build a website that's better tailored
+                to help me memorize new vocabulary and kanji. I hope to have this Japanese Practice Website completed and deployed to Vercel by the end of September.
+
+                And outside of software development, my favorite hobbies are playing video games (of course), learning Japanese, listening to music, watching sports, and driving! My favorite musicians are Danny Brown, JPEGMAFIA, Tyler The Creator, and hundreds of others.
+                As for my favorite sports teams, the Los Angeles Rams are my NFL team, and of course, the Nebraska Cornhuskers are my college sports team all the way.
+
+                Thank you for reading this far! Unless you skimmed to the very bottom, I guess. But either way, if you'd like to connect, message me on LinkedIn or shoot me an email! :D
+            </p>
         </div>
     );
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,18 +1,45 @@
 import '../index.css'
+import { Link } from 'react-router-dom';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 function LandingPage() {
     return (
-        <div className="flex flex-col gap-10 border-1 px-30 mx-10 my-10 py-10">
+        <div className="flex flex-col gap-10 border-1 px-30 py-10 mx-10 my-10 [@media(min-width:930px)]:mx-10">
             <h1 className="text-center text-4xl font-[TASA Orbiter]">Gage Witcig's Portfolio</h1>
-            <div className="flex flex-row gap-20">
+            <div className="flex flex-col [@media(min-width:930px)]:flex-row gap-20">
                 <img
-                    src="../src/assets/ML6A4928.jpg"
+                    src="../../src/assets/ML6A4928.jpg"
                     alt="Picture of Gage Witcig at the University of Nebraska-Lincoln's Love Library"
-                    className="h-200"
+                    className="h-180 w-120 object-contain order-2 [@media(min-width:930px)]:order-1"
                 ></img>
-                <div className="flex justify-center items-center">
-                    <h1 className="text-3xl font-[TASA Orbiter]">Under construction...</h1>
+                <div className="flex flex-col gap-20 order-2 justify-center items-center">
+                    <div className="flex flex-col text-3xl gap-15 justify-center items-center">
+                        <Link to="/about">
+                            About Me
+                        </Link>
+                        <Link to="/projects">
+                            My Projects
+                        </Link>
+                        <Link to="/resume">
+                            My Resume
+                        </Link>
+                    </div>
+                    <div className="flex flex-col gap-5">
+                        <p className="text-center text-xl font-[TASA Orbiter]">Socials:</p>
+                        <div className="flex flex-row gap-10 justify-center items-center">
+                            <a href="https://www.linkedin.com/in/gage-witcig-a96722373/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+                                <FaLinkedin className="w-15 h-15 hover:scale-110 transition-transform"/>
+                            </a>
+                            <a href="https://github.com/gwitcig2" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+                                <FaGithub className="w-15 h-15 hover:scale-110 transition-transform"/>
+                            </a>
+                            <a href="mailto:gagewitcig@gmail.com" aria-label="Email">
+                                <FaEnvelope className="w-15 h-15 hover:scale-110 transition-transform"/>
+                            </a>
+                        </div>
+                    </div>
                 </div>
+
             </div>
 
         </div>
